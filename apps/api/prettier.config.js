b26dcda01b481a4de017dd7825d0aec575062bea
Mitchell Sutton <mitchellsutton2@gmail.com>
2025-12-15 19:28:39 +1100

    Added prettier

---
+++ apps/api/prettier.config.js
@@ -0,0 +1,3 @@
+import companyPrettierConfig from '@repo/eslint-config/prettier';
+
+export default companyPrettierConfig;
---
+++ packages/eslint-config/prettier.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+};
+
+export default config;
